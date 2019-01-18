c1d45bf25604de7ab272b5f8492815ec42fcc7fe
jsjimenez51 <jsjimenez51@gmail.com>
2019-01-18 03:22:28 +0000

    ADD: file 0-create_a_file.pp

---
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Uses Puppet to create a file 
+
+file { 'holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
