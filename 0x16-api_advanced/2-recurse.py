f585d7b85f3c8b2c1f08cb4ed4d15c940dc33824
jsjimenez51 <jsjimenez51@gmail.com>
2019-02-13 14:50:33 -0800

    UPDATE: corrected pagination and pep8

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,23 +5,23 @@ Sends a query to an api to retrieve data
 import requests
 
 
-def recurse(subreddit, hot_list[]):
+def recurse(subreddit, after='', hot_list=[]):
     """
     Queries Reddit API and returns titles for hot posts for a given sub
     """
     url = 'https://api.reddit.com/r/{}/hot'.format(subreddit)
     headers = {'User-Agent': 'Hot Post Recursive Grab Info'}
-
+    paginate = {'after': after}
     try:
-        reddit_data = requests.get(url, headers=headers, params={'after':
-                                   after}, allow_redirects=False).json()
+        reddit_data = requests.get(url, headers=headers, params=paginate,
+                                   allow_redirects=False).json()
 
         all_articles = reddit_data['data']['children']
 
         for article in all_articles:
-            hot_list.appent(article['data']['title'])
+            hot_list.append(article['data']['title'])
 
-        next_pg = reddit_data['data']['title']
+        next_pg = reddit_data['data']['after']
 
         if next_pg is None:
             return hot_list
