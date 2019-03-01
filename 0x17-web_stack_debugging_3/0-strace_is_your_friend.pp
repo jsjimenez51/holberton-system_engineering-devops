a41b456c52c57710521867416696ed2ab62ceabf
jsjimenez51 <jsjimenez51@gmail.com>
2019-03-01 07:38:07 -0800

    UPDATE: puppet-lint fixes

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix to a Wordpress settings file for the proper extension
 
 exec { 'wp setting config':
+  command  => 'sed -i \'s/phpp/php/\' /var/www/html/wp-settings.php',
   provider => 'shell',
-  command => 'sed -i \'s/phpp/php/\' /var/www/html/wp-settings.php'
 }
